7e01a2c104b8ec58636af9ffda373d3b69ffdd12
francisco-kpodotay <135797234+francisco-kpodotay@users.noreply.github.com>
2023-10-13 09:20:39 +0200

    friday 9.00

---
+++ backend/server.js
@@ -0,0 +1,88 @@
+import express from "express";
+import fs from "fs/promises";
+
+import path from "path";
+import { fileURLToPath } from "url";
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use((req, res, next) => {
+  console.log(req.path, req.method);
+  next();
+});
+
+app.use(
+  "/edit/users",
+  express.static(path.join(__dirname, "..", "frontend/home"))
+);
+
+app.get("/api/users", async (req, res) => {
+  try {
+    const rawData = await fs.readFile("./data.json", "utf8");
+    const data = JSON.parse(rawData);
+    res.send(data);
+  } catch (error) {
+    console.log(error);
+  }
+});
+
+app.post("/api/users", async (req, res) => {
+  console.log(req.body.name);
+  try {
+    const rawData = await fs.readFile("./data.json", "utf8");
+    const data = JSON.parse(rawData);
+    const userIds = data.map((user) => user.id);
+    let maxId = 0;
+    if (userIds.length === 0) {
+      maxId = 0;
+    } else {
+      maxId = Math.max(...userIds);
+    }
+    const newUser = {
+      id: maxId + 1,
+      name: req.body.name,
+      age: req.body.age,
+    };
+    data.push(newUser);
+    await fs.writeFile("./data.json", JSON.stringify(data), "utf8");
+    res.redirect("/edit/users");
+  } catch (error) {
+    console.log(error);
+  }
+});
+
+app.put("/api/users/:id", async (req, res) => {
+  try {
+    const rawData = await fs.readFile("./data.json", "utf8");
+    const data = JSON.parse(rawData);
+    const user = data.reduce((good, all) => {
+      return good.id === +req.body.id ? good : all
+    })
+    user.name = req.body.name;
+    user.age = req.body.age;
+    await fs.writeFile("./data.json", JSON.stringify(data), "utf8");
+    res.sendStatus(200)
+  } catch (error) {
+    console.log(error);
+  }
+});
+
+app.delete("/api/users/:id", async (req, res) => {
+  const rawData = await fs.readFile("./data.json", "utf8");
+  const data = JSON.parse(rawData);
+  const newData = data.filter((user) => {
+    return user.id !== +req.params.id;
+  });
+  await fs.writeFile("./data.json", JSON.stringify(newData), "utf8");
+  res.sendStatus(200);
+});
+
+app.patch("/api/users/:id", (req, res) => {});
+
+app.listen(3000, () => {
+  console.log("listen on: http://localhost:3000/edit/users");
+});
---
+++ frontend/home/app.js
@@ -0,0 +1,93 @@
+const loadEvent = () => {
+  fetchData("http://localhost:3000/api/users/").then((data) => {
+    listAll(data);
+  });
+};
+window.addEventListener("load", loadEvent);
+
+async function fetchData(url) {
+  try {
+    const rawData = await fetch(url);
+    const data = await rawData.json();
+    return data;
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+function createHTMLElement(elementHTML, content = "", id = "") {
+  const element = document.createElement(elementHTML);
+  element.innerHTML = content;
+  element.id = id;
+  return element;
+}
+
+function listAll(data) {
+  const root = document.querySelector("#root");
+  root.innerHTML = "";
+  data.map((user) => {
+    const userDiv = createHTMLElement("div");
+    userDiv.append(createHTMLElement("li", `ID: ${user.id}`));
+    userDiv.append(createHTMLElement("li", `Name: ${user.name}`));
+    userDiv.append(createHTMLElement("li", `Age: ${user.age}`));
+    // Delete button
+    const deleteButton = createHTMLElement("button", "DELETE");
+    deleteButton.addEventListener("click", async () => {
+      try {
+        await fetch(`http://localhost:3000/api/users/${user.id}`, {
+          method: "DELETE",
+        });
+        loadEvent();
+      } catch (error) {
+        console.log(error);
+      }
+    });
+    userDiv.append(deleteButton);
+    // editing user (PUt)
+    const editUserBtn = createHTMLElement("button", "Edit user (PUT)");
+    editUserBtn.addEventListener("click", () => {
+      document.querySelector(`#put${user.id}`).style.display = "block";
+    });
+    userDiv.append(editUserBtn);
+    userDiv.append(createModal("put", user.id));
+
+    root.append(userDiv);
+  });
+}
+
+function createModal(nameOfModal, id) {
+  const form = createHTMLElement("form");
+  form.id = nameOfModal + id;
+  const nameLabel = createHTMLElement("label", "Name:", "nameLabel");
+  form.appendChild(nameLabel);
+  const nameInput = createHTMLElement("input");
+  nameInput.name = "name";
+  form.appendChild(nameInput);
+  const ageLabel = createHTMLElement("label", "Age:", "ageLabel");
+  form.appendChild(ageLabel);
+  const ageInput = createHTMLElement("input");
+  ageInput.name = "age";
+  form.appendChild(ageInput);
+  const submitButton = createHTMLElement("input", "Submit");
+  submitButton.type = "submit";
+
+  submitButton.addEventListener("click", async (e) => {
+    e.preventDefault();
+    try {
+      let newData = { id: id, name: nameInput.value, age: ageInput.value };
+      console.log(newData);
+      const respons = await fetch(`http://localhost:3000/api/users/${id}`, {
+        method: "PUT",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify(newData),
+      });
+      loadEvent();
+    } catch (error) {
+      console.log(error);
+    }
+  });
+
+  form.appendChild(submitButton);
+  form.style.display = "none";
+  return form;
+}
